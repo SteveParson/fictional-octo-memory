f4dc79b6dbf0f3ef75266c87c81a7bf2a187cb3a
Steve Parson <me@steveparson.ca>
2023-09-12 21:17:56 -0300

    feat: deploy ec2 instance

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -14,12 +14,20 @@ data "aws_ami" "al2" {
   owners = ["099720109477"]
 }
 
-# resource "aws_instance" "my_instance" {
-#   ami                    = data.aws_ami.al2.id
-#   instance_type          = "t2.micro"
-#   key_name               = aws_key_pair.my_key.key_name
-#   vpc_security_group_ids = [aws_security_group.my_sg.id]
-# }
+resource "aws_instance" "my_instance" {
+  ami                    = data.aws_ami.al2.id
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.my_key.key_name
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
+}
+
+resource "aws_route53_record" "ec2_instance_a_record" {
+  zone_id = aws_route53_zone.aws_subdomain.zone_id
+  name    = "proxy.aws.steveparson.ca"
+  type    = "A"
+  ttl     = "300"
+  records = [aws_instance.my_instance.public_ip]
+}
 
 resource "aws_key_pair" "my_key" {
   key_name   = "my_key"
@@ -52,11 +60,6 @@ resource "aws_security_group" "my_sg" {
   }
 }
 
-# output "instance_public_ip" {
-#   description = "The public IP of the launched instance"
-#   value       = aws_instance.my_instance.public_ip
-# }
-
 resource "aws_route53_zone" "aws_subdomain" {
   name = "aws.steveparson.ca"
 }
@@ -64,14 +67,6 @@ output "name_servers" {
   description = "The NS records for the hosted zone"
   value       = aws_route53_zone.aws_subdomain.name_servers
 }
-
-# resource "aws_route53_record" "ec2_instance_a_record" {
-#   zone_id = aws_route53_zone.aws_subdomain.zone_id
-#   name    = "proxy.aws.steveparson.ca"
-#   type    = "A"
-#   ttl     = "300"
-#   records = [aws_instance.my_instance.public_ip]
-# }
 
 output "aws_subdomain_ns" {
   value       = aws_route53_zone.aws_subdomain.name_servers
