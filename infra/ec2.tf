561569a5c55c580632a6c3b3e3ef8f0fdaad0c16
Steve Parson <me@steveparson.ca>
2023-09-12 21:50:19 -0300

    feat: remove ec2 isntance

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -14,20 +14,20 @@ data "aws_ami" "al2" {
   owners = ["099720109477"]
 }
 
-resource "aws_instance" "my_instance" {
-  ami                    = data.aws_ami.al2.id
-  instance_type          = "t2.micro"
-  key_name               = aws_key_pair.my_key.key_name
-  vpc_security_group_ids = [aws_security_group.my_sg.id]
-}
+# resource "aws_instance" "my_instance" {
+#   ami                    = data.aws_ami.al2.id
+#   instance_type          = "t2.micro"
+#   key_name               = aws_key_pair.my_key.key_name
+#   vpc_security_group_ids = [aws_security_group.my_sg.id]
+# }
 
-resource "aws_route53_record" "ec2_instance_a_record" {
-  zone_id = aws_route53_zone.aws_subdomain.zone_id
-  name    = "proxy.aws.steveparson.ca"
-  type    = "A"
-  ttl     = "300"
-  records = [aws_instance.my_instance.public_ip]
-}
+# resource "aws_route53_record" "ec2_instance_a_record" {
+#   zone_id = aws_route53_zone.aws_subdomain.zone_id
+#   name    = "proxy.aws.steveparson.ca"
+#   type    = "A"
+#   ttl     = "300"
+#   records = [aws_instance.my_instance.public_ip]
+# }
 
 resource "aws_key_pair" "my_key" {
   key_name   = "my_key"
